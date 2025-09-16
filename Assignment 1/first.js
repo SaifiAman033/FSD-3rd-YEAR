735d8ee2d3399a910eef81cd81f87c158623374d
SaifiAman033 <Amansaif542@gmail.com>
2025-09-16 09:04:34 +0530

    first javascript

---
+++ Assignment 1/first.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+const price1 = 5;
+const price2 = 10;
+const total = price1 + price2;
+console.log("Total is: " + total);
+let x = 5;
+let y = 10;
+const total1=x+y;
+console.log("Total is: " + total1);
+
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(20,30));
